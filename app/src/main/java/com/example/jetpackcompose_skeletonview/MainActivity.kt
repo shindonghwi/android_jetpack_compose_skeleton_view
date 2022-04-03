f373c16b49152312aad607620fe0efc0e8bb9648
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-04 00:56:44 +0900

    feat: 배포된 SkeletonView 테스트 완료

--- app/src/main/java/com/example/jetpackcompose_skeletonview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_skeletonview/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.jetpackcompose_skeletonview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose_skeletonview.ui.theme.JetpackCompose_SkeletonViewTheme
+import dev.wolf.skeleton_ui.SkeletonView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackCompose_SkeletonViewTheme {}
+            JetpackCompose_SkeletonViewTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    SkeletonView(
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        repeatDelayTime = 100
+                    )
+                }
+            }
         }
     }
 }
