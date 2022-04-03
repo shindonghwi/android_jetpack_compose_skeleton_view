b79509482ea59bf7a198f2fc6886a257f84496f9
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-04 01:07:24 +0900

    feat: Skeleton 샘플 화면 생성

--- app/src/main/java/com/example/jetpackcompose_skeletonview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_skeletonview/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -16,15 +18,69 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose_SkeletonViewTheme {
-                Box(modifier = Modifier.fillMaxSize()) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp)
+                ) {
+
                     SkeletonView(
                         modifier = Modifier
-                            .padding(20.dp)
                             .fillMaxWidth()
                             .height(100.dp)
                             .clip(RoundedCornerShape(8.dp)),
                         repeatDelayTime = 100
                     )
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        items(5) {
+                            SkeletonView(
+                                modifier = Modifier
+                                    .size(80.dp)
+                                    .clip(RoundedCornerShape(8.dp)),
+                                repeatDelayTime = 100
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    SkeletonView(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(24.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        repeatDelayTime = 100
+                    )
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+
+                    LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        items(5) {
+                            SkeletonView(
+                                modifier = Modifier
+                                    .width(130.dp)
+                                    .height(250.dp)
+                                    .clip(RoundedCornerShape(8.dp)),
+                                repeatDelayTime = 100
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        SkeletonView(
+                            modifier = Modifier
+                                .size(130.dp)
+                                .clip(RoundedCornerShape(100.dp)),
+                            repeatDelayTime = 100
+                        )
+                    }
                 }
             }
         }
